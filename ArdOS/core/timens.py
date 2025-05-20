08477abee94676d184a79aa62913c750ce763877
Ардиновец <149672990+maxrazrabruua@users.noreply.github.com>
2025-05-21 01:55:19 +0300

    Основная папка проекта
    24 мб озу минимум
    32 мб озу желательно

---
+++ ArdOS/core/timens.py
@@ -0,0 +1,12 @@
+import time
+
+class Timer:
+    """Таймер отсчёта времени"""
+    def __init__(self, new: float):
+        self.tm = time.time()
+        self.new = new
+    
+    def __call__(self):
+        if self.tm + self.new <= time.time():
+            return True
+        return False
